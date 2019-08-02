b453bbd35d774f8c6a70968fbed449700d6fba8e
Onaya [P] <do.hoangnam9x@gmail.com>
2019-08-02 22:31:00 +0700

    finally got node-pty

--- src/app/shared/states/x-terminal/x-terminal.service.ts
+++ src/app/shared/states/x-terminal/x-terminal.service.ts
@@ -1,18 +1,44 @@
-import { Injectable } from '@angular/core';
+import { HostListener, Injectable } from '@angular/core';
 import { XTerminalStore } from './x-terminal.store';
-import { HttpClient } from '@angular/common/http';
 import { XTerminalQuery } from './x-terminal.query';
-import { Terminal } from 'xterm';
+import { IDisposable as xtermIDisposable, Terminal } from 'xterm';
 import * as os from 'os';
+import * as pty from 'node-pty';
+import { IDisposable as ptyIDisposable } from 'node-pty';
+import * as fit from 'xterm/lib/addons/fit/fit';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class XTerminalService {
 
+  private isResizing: Subject<any> = new Subject<any>();
+
+  private ptyDataListener: ptyIDisposable = null;
+  private xtermDataListener: xtermIDisposable = null;
+  private xtermResizeListener: xtermIDisposable = null;
+
   constructor(
     private xTerminalStore: XTerminalStore,
     private xTerminalQuery: XTerminalQuery,
-    private http: HttpClient
   ) {
+    this.isResizing.pipe(
+      debounceTime(20),
+    ).subscribe(event => {
+      if (!!event) {
+        this.fitTerminal();
+      }
+    });
+
+    this.xTerminalQuery.select().subscribe(
+      terminalState => {
+        if (!!terminalState.terminal && !!terminalState.pty) {
+          this.startListenerTerminal(terminalState.terminal, terminalState.pty);
+        } else {
+          this.stopListenerTerminal();
+        }
+      }
+    );
   }
 
   openTerminal(terminalDOM: HTMLElement) {
@@ -27,31 +53,9 @@ export class XTerminalService {
       rows: terminal.rows
     });
 
-    const fitDebounced = _debounce(() => {
-      (term as any).fit();
-    }, 17);
-
-    term.on('data', (data: string) => {
-      ptyProc.write(data);
-    });
-
-    term.on('resize', size => {
-      ptyProc.resize(
-        Math.max(size ? size.cols : term.cols, 1),
-        Math.max(size ? size.rows : term.rows, 1)
-      );
-    });
-
-    ptyProc.on('data', data => {
-      term.write(data);
-    });
-
-    window.onresize = () => {
-      fitDebounced();
-    };
-
     this.xTerminalStore.update({
-      terminal: terminal
+      terminal: terminal,
+      pty: ptyProc
     });
   }
 
@@ -80,5 +84,47 @@ export class XTerminalService {
     this.xTerminalStore.update({
       current_dir: directory
     });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowsResize(event) {
+    this.isResizing.next(event);
+  }
+
+  private startListenerTerminal(terminalInstance: Terminal, ptyInstance: pty.IPty) {
+    this.xtermDataListener = terminalInstance.onData((data: string) => {
+      ptyInstance.write(data);
+    });
+    this.xtermResizeListener = terminalInstance.onResize(size => {
+      ptyInstance.resize(
+        Math.max(size ? size.cols : terminalInstance.cols, 1),
+        Math.max(size ? size.rows : terminalInstance.rows, 1)
+      );
+    });
+    this.ptyDataListener = ptyInstance.onData(data => {
+      terminalInstance.write(data);
+    });
+  }
+
+
+  private stopListenerTerminal() {
+    if (!!this.xtermDataListener) {
+      this.xtermDataListener.dispose();
+    }
+
+    if (!!this.xtermResizeListener) {
+      this.xtermResizeListener.dispose();
+    }
+
+    if (!!this.ptyDataListener) {
+      this.ptyDataListener.dispose();
+    }
+  }
+
+  private fitTerminal() {
+    const terminal = this.xTerminalStore;
+    if (terminal instanceof Terminal) {
+      fit.fit(terminal);
+    }
   }
 }
--- src/app/shared/states/x-terminal/x-terminal.store.ts
+++ src/app/shared/states/x-terminal/x-terminal.store.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Store, StoreConfig } from '@datorama/akita';
 import { Terminal } from 'xterm';
+import * as pty from 'node-pty';
 
 export interface XTerminalState {
   is_open: boolean;
   current_dir?: string;
   terminal?: Terminal;
+  pty?: pty.IPty;
 }
 
 export function createInitialState(): XTerminalState {
