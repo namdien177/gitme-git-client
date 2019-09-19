4b793270aa56473d4a104f8025cc670a33479a6e
Onaya [W-P] <do.hoangnam9x@gmail.com>
2019-09-20 01:12:01 +0700

    reformat

--- src/app/shared/pipe/safe-html.pipe.spec.ts
+++ src/app/shared/pipe/safe-html.pipe.spec.ts
@@ -1,8 +1,8 @@
 import { SafeHtmlPipe } from './safe-html.pipe';
 
 describe('SafeHtmlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SafeHtmlPipe();
-    expect(pipe).toBeTruthy();
-  });
+    it('create an instance', () => {
+        const pipe = new SafeHtmlPipe();
+        expect(pipe).toBeTruthy();
+    });
 });
