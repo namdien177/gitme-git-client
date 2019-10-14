900797c0f2ad2a40760fb090e0f01984dc01aec1
Onaya <do.hoangnam9x@gmail.com>
2019-10-15 00:53:30 +0700

    first test

--- src/app/shared/components/branch/branch-item/branch-item.component.ts
+++ src/app/shared/components/branch/branch-item/branch-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { RepositoryBranchSummary } from '../../../state/DATA/repository-branches';
+import { RepositoryBranchesService, RepositoryBranchSummary } from '../../../state/DATA/repository-branches';
+import { RepositoryStatusService } from '../../../state/DATA/repository-status';
+import { RepositoriesService, Repository } from '../../../state/DATA/repositories';
+import { StatusSummary } from '../../../model/statusSummary.model';
 
 @Component({
     selector: 'gitme-branch-item',
@@ -9,11 +12,43 @@ import { RepositoryBranchSummary } from '../../../state/DATA/repository-branches
 export class BranchItemComponent implements OnInit {
 
     @Input() branchSummary: RepositoryBranchSummary;
+    private repository: Repository = null;
+    private status: StatusSummary = null;
 
-    constructor() {
+    constructor(
+        private repositoryBranchService: RepositoryBranchesService,
+        private repositoryStatusService: RepositoryStatusService,
+        private repositoriesService: RepositoriesService
+    ) {
+        this.repositoriesService.selectActive(false)
+        .subscribe(repo => {
+            this.repository = repo;
+        });
+
+        this.repositoryStatusService.select()
+        .subscribe(status => {
+            this.status = status;
+        });
     }
 
     ngOnInit() {
     }
 
+    checkoutBranches() {
+        if (this.repository && this.branchSummary) {
+            if (this.status.files.length > 0) {
+                // there is file changes
+                // stop the checkout
+            } else {
+                this.repositoriesService.checkoutBranch(
+                    this.repository,
+                    this.branchSummary
+                ).subscribe(
+                    status => {
+                        console.log(status);
+                    }
+                );
+            }
+        }
+    }
 }
