3b29f8ae27f5df99b962c682c0a479927e5d9be3
do.hoangnam9x <do.hoangnam9x@gmail.com>
2019-11-05 00:35:00 +0700

    update dialog for new branch

--- src/app/shared/components/branch/_dialogs/branch-new/branch-new-option/branch-new-option.component.ts
+++ src/app/shared/components/branch/_dialogs/branch-new/branch-new-option/branch-new-option.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { FormBuilder } from '@angular/forms';
+import { YesNoDialogModel } from '../../../../../model/yesNoDialog.model';
+import { Repository } from '../../../../../state/DATA/repositories';
+import { FileStatusSummaryView, RepositoryStatusService } from '../../../../../state/DATA/repository-status';
+import { RepositoryBranchSummary } from '../../../../../state/DATA/repository-branches';
 
 @Component({
-  selector: 'gitme-branch-new-option',
-  templateUrl: './branch-new-option.component.html',
-  styleUrls: ['./branch-new-option.component.scss']
+    selector: 'gitme-branch-new-option',
+    templateUrl: './branch-new-option.component.html',
+    styleUrls: ['./branch-new-option.component.scss']
 })
 export class BranchNewOptionComponent implements OnInit {
 
-  constructor() { }
+    repository: Repository;
+    branch: RepositoryBranchSummary;
+    currentChanges: FileStatusSummaryView[];
 
-  ngOnInit() {
-  }
+    constructor(
+        public dialogRef: MatDialogRef<BranchNewOptionComponent>,
+        private repositoryStatusService: RepositoryStatusService,
+        private fb: FormBuilder,
+        @Inject(MAT_DIALOG_DATA) public emittedDataFromView: YesNoDialogModel
+    ) {
+        this.repository = emittedDataFromView.data['repository'];
+        this.branch = emittedDataFromView.data['branch'];
+        repositoryStatusService.select().subscribe(
+            statusChanges => this.currentChanges = statusChanges.files
+        );
+    }
 
+    ngOnInit() {
+    }
+
+    cancel() {
+        this.dialogRef.close(null);
+    }
 }
--- src/app/shared/components/branch/list-branches/list-branches.component.ts
+++ src/app/shared/components/branch/list-branches/list-branches.component.ts
@@ -1,41 +1,81 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { RepositoryBranchesService, RepositoryBranchSummary } from '../../../state/DATA/repository-branches';
 import { RepositoriesService, Repository } from '../../../state/DATA/repositories';
+import { MatDialog } from '@angular/material';
+import { BranchNewOptionComponent } from '../_dialogs/branch-new/branch-new-option/branch-new-option.component';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { YesNoDialogModel } from '../../../model/yesNoDialog.model';
 
 @Component({
     selector: 'gitme-repo-branches',
     templateUrl: './list-branches.component.html',
     styleUrls: ['./list-branches.component.scss']
 })
-export class ListBranchesComponent implements OnInit {
+export class ListBranchesComponent implements OnInit, AfterViewInit {
 
     repository: Repository = null;
     branch: RepositoryBranchSummary = null;
 
     @Input()
     branches: RepositoryBranchSummary[] = [];
-
     @Output()
     branchesChange: EventEmitter<RepositoryBranchSummary[]> = new EventEmitter<RepositoryBranchSummary[]>();
 
     constructor(
         private repositoryBranchesService: RepositoryBranchesService,
-        private repositoriesService: RepositoriesService
+        private repositoriesService: RepositoriesService,
+        private matDialog: MatDialog
     ) {
         this.repository = this.repositoriesService.getActive();
         this.branch = this.repositoryBranchesService.getActive();
+    }
+
+    ngAfterViewInit(): void {
+        this.openOptionNewBranch();
     }
 
     ngOnInit() {
     }
 
     openOptionNewBranch() {
-        this.repositoryBranchesService.newBranchFrom(
-            this.repository,
-            'test-create-2',
-            this.branch
-        ).subscribe(result => {
-            console.log(result);
-        });
+        const data: YesNoDialogModel = {
+            title: 'New Branch',
+            body: null,
+            data: {
+                repository: this.repository,
+                branch: this.branch
+            },
+            decision: {
+                noText: 'Cancel',
+                yesText: 'Create'
+            }
+        };
+        const dialogOpenInitial = this.matDialog.open(
+            BranchNewOptionComponent, {
+                width: '380px',
+                height: '350px',
+                data: data,
+                panelClass: 'bg-primary-black-mat-dialog',
+            }
+        );
+
+        dialogOpenInitial.afterClosed().pipe(
+            switchMap(newBranch => {
+                if (newBranch) {
+
+                }
+
+                return of(null);
+            })
+        );
+
+        // this.repositoryBranchesService.newBranchFrom(
+        //     this.repository,
+        //     'test-create-2',
+        //     this.branch
+        // ).subscribe(result => {
+        //     console.log(result);
+        // });
     }
 }
